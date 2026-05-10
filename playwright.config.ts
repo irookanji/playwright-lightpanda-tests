ccfc21718ec63e15222503b717af86a1881fed6f
Bakhtiyar Zubairov <irookanji@gmail.com>
2026-05-10 10:23:07 +0200

    chore: modify Pw config and GH Actions workflow for improved reporting
    - Adjusts the reporter configuration in playwright.config.ts to prioritize HTML over dot format in CI
    - Updates the GitHub Actions workflow to include shard index in the artifact name and added a condition to ignore if no files are found

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'dot' : [['html'], ['list']],
+  reporter: process.env.CI ? [['html'], ['dot']] : [['html'], ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
